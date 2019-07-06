1807a5dd6c09180772a3aab21e775c9cc30bbd70
Jc-Sir <1334959005@qq.com>
2019-07-06 14:53:47 +0800

    Create dart数据类型之间的转换.dart

---
+++ dataType/dart数据类型之间的转换.dart
@@ -0,0 +1,32 @@
+/*
+    1、Number 与 String 之间的转化
+
+      Number 转换为String类型  toString()
+
+      String 转换为Number类型   int.parse()  /  double.parse()
+
+      String str = '1234560';
+
+      var mynum = int.parse(str);
+
+      var Num = 123456;
+
+      var str = Num.toString();
+
+
+      2、其他类型转换为boolean
+      //isEmpty: 判断字符串是否为空
+
+      var str = '';
+
+      if(str.isEmpty){
+        print('str为空');
+      }
+      else{
+          print('str不为空');
+      }
+
+
+
+
+*/
